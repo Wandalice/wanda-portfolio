e7ef03f362f8675c1b11c977233a64d2a0965869
Wandalice <61400002+Wandalice@users.noreply.github.com>
2025-01-07 17:12:37 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/wanda-portfolio/', // Dein Repository-Name
+  plugins: [react()],
+})
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
